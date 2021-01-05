3151081f2ca05c02395662460c7df9da6c315a63
dpoeira <diogopoeira91@gmail.com>
2021-01-05 19:17:38 +0000

    Fix sonar issues

--- src/lib/ops/skip.ts
+++ src/lib/ops/skip.ts
@@ -2,6 +2,8 @@ import {Yield} from '../yield';
 import {Operation} from '../operation';
 import {Sequence} from '../sequence';
 
+const DO_NOTHING = () => {}; // nosonar
+
 export class Skip<T> implements Operation<T> {
     private readonly upstream: Sequence<T>;
     private readonly n: number;
@@ -15,14 +17,14 @@ export class Skip<T> implements Operation<T> {
 
     tryAdvance(yld: Yield<T>): boolean {
         for (; this.index < this.n; this.index++) {
-            this.upstream.adv.tryAdvance(() => {});
+            this.upstream.adv.tryAdvance(DO_NOTHING);
         }
         return this.upstream.adv.tryAdvance(yld);
     }
 
     traverse(yld: Yield<T>): void {
         for (; this.index < this.n; this.index++) {
-            this.upstream.adv.tryAdvance(() => {});
+            this.upstream.adv.tryAdvance(DO_NOTHING);
         }
         this.upstream.trv.traverse(yld);
     }
